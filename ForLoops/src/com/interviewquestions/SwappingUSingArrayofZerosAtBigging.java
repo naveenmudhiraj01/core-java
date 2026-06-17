e13ef36112ccc5c113072e7e1e06ba60c9748184
Naveen_Dasari <na.dasari@ajacs.in>
2026-06-17 11:07:43 +0530

    loops added

---
+++ ForLoops/src/com/interviewquestions/SwappingUSingArrayofZerosAtBigging.java
@@ -0,0 +1,21 @@
+package com.interviewquestions;
+
+public class SwappingUSingArrayofZerosAtBigging {
+	public static void main(String[] args) {
+		int arr[]= {0,1,7,0,4,0,9,0};
+		int j=0;
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i]!=0) {
+				int temp=arr[i];
+				arr[i]=arr[j];
+				arr[j]=temp;
+				j++;
+			}
+		}
+		for(int num:arr) {
+			System.out.print(num+" ");
+		}
+		
+	}
+
+}
